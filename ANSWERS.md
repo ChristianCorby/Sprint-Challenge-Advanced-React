93a93d79d204d0d145d515a2643635911da786c5
Christian <christiancorby@gmail.com>
2020-04-10 13:56:03 -0400

    Finished

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,4 +1,16 @@
 - [ ] Why would you use class component over function components (removing hooks from the question)?
+
+Class components are used to implement logic or state, and use lifecycle methods.
+
 - [ ] Name three lifecycle methods and their purposes.
+
+Constructors are used to initialize and load data.
+Render, renders and gives JSX code to the Dom
+
 - [ ] What is the purpose of a custom hook?
+
+custom hooks make our code more reusable for for our project
+
 - [ ] Why is it important to test our apps?
+
+Test for bugs, and prepare your code for production.
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import Players from './players';
+import useDarkMode from './hooks/useDarkMode';
+import { useFetchPlayers } from './hooks/useFetchPlayers';
+it('Renders App without crashing', () => {
+  render(<App />);
 });
